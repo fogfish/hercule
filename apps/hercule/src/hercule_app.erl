fa53f4d6054678b0102a98e784f5f42e40ce020a
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2016-09-06 21:18:49 +0300

    enable rel assembly

--- apps/hercule/src/hercule_app.erl
+++ apps/hercule/src/hercule_app.erl
@@ -1,3 +1,19 @@
+%%
+%%   Copyright (c) 2016, Dmitry Kolesnikov
+%%   All Rights Reserved.
+%%
+%%   Licensed under the Apache License, Version 2.0 (the "License");
+%%   you may not use this file except in compliance with the License.
+%%   You may obtain a copy of the License at
+%%
+%%       http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%   Unless required by applicable law or agreed to in writing, software
+%%   distributed under the License is distributed on an "AS IS" BASIS,
+%%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%   See the License for the specific language governing permissions and
+%%   limitations under the License.
+%%
 -module(hercule_app).
 -behaviour(application).
 
@@ -9,7 +25,6 @@
 %%
 %%
 start(_Type, _Args) ->
-   ets:new(hercule, [set, public, named_table]),
    config(),
    hercule_sup:start_link(). 
 
@@ -18,16 +33,46 @@ start(_Type, _Args) ->
 stop(_State) ->
    ok.
 
+
 %%
 %%
 config() ->
-   {ok, Schema} = esio:socket( opts:val(storage, hercule) ),
+   datalog_db(),
+   config_ns_schema( opts:val(ns, hercule) ),
+   config_ns_schema( opts_env() ).
+
+
+%%
+%%
+config_ns_schema(List) ->
+   {ok, Schema} = esio:socket( storage() ),
    lists:foreach(
       fun(X) ->
          elasticnt:schema(Schema, X, [])
       end,
-      opts:val(ns, hercule)
+      List
    ),
    esio:close(Schema).
 
+
+%%
+%%
+storage() ->
+   os:getenv("HERCULE_STORAGE", opts:val(storage, hercule)).
+
+%%
+%%
+opts_env() ->
+   N = scalar:i( os:getenv("HERCULE_NS", 0) ),
+   lists:map(
+      fun(X) ->
+          os:getenv("HERCULE_NS_" ++ scalar:c(X))
+      end,
+      lists:seq(1, N)
+   ).
+
+%%
+%% create local storage for datalog statements
+datalog_db() ->
+   ets:new(hercule, [set, public, named_table]).
 
---
+++ rel/deploy.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+##
+##   Copyright (c) 2016, Dmitry Kolesnikov
+##   All Rights Reserved.
+##
+##   Licensed under the Apache License, Version 2.0 (the "License");
+##   you may not use this file except in compliance with the License.
+##   You may obtain a copy of the License at
+##
+##       http://www.apache.org/licenses/LICENSE-2.0
+##
+##   Unless required by applicable law or agreed to in writing, software
+##   distributed under the License is distributed on an "AS IS" BASIS,
+##   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##   See the License for the specific language governing permissions and
+##   limitations under the License.
+##
+## @doc
+##    node deployment script
+##       ${PREFIX} - root installation folder
+##       ${REL}    - absolute path to release
+##       ${APP}    - application name
+##       ${VSN}    - application version
+set -u
+set -e
+
+##
+## discover version of erlang release
+VERSION=`cat ${REL}/releases/start_erl.data`
+SYS_VSN=${VERSION% *}
+APP_VSN=${VERSION#* }
+
+##
+## make alias to current version
+rm -f ${PREFIX}/${APP}
+ln -s ${PREFIX}/${APP}-${VSN} ${PREFIX}/${APP}
+
+##
+## build service wrapper
+if [[ $(uname -s) == "Linux" ]] ;
+then
+
+cat > /etc/init.d/${APP} <<- EOF
+#!/bin/sh
+export HOME=/root
+
+FILE=${REL}/releases/${APP_VSN}/vm.args
+HOST=\$(curl -s --connect-timeout 1 http://169.254.169.254/latest/meta-data/local-ipv4 || echo "127.0.0.1")
+NODE=\$(sed -n -e "s/-name \(.*\)@.*/\1/p" \${FILE})
+sed -i -e "s/@\(127.0.0.1\)/@\${HOST}/g" \${FILE}
+
+${PREFIX}/${APP}/bin/${APP} \$1
+EOF
+   
+chmod ugo+x /etc/init.d/${APP}
+fi
+
+set +u
+set +e
+
+## EOF
